7ff9aabc1d27d67c3fc7dba64b8438fa5c9c4d15
oluwadolapo <oluwadolapooni@gmail.com>
2020-10-08 06:00:47 +0100

    minor changes

--- summarizer/rnn/train_eval.py
+++ summarizer/rnn/train_eval.py
@@ -51,8 +51,10 @@ def train(args, batches, encoder, decoder, encoder_optimizer,
     # Forward batch of sequences through decoder one step at a time
     if use_teacher_forcing:
       for t in range(max_target_len):
-        #decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden, encoder_outputs)
-        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+        if args.with_attention:
+          decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden, encoder_outputs)
+        else:
+          decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
         # Teacher forcing: next input is current target
         decoder_input = target_variable[t].view(1, -1)
         #Calculate and accumulate loss
@@ -101,8 +103,10 @@ def validate(args, batches, encoder, decoder, device):
     # Forward batch of sequences through decoder one step at a time
     if use_teacher_forcing:
       for t in range(max_target_len):
-        #decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden, encoder_outputs)
-        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+        if args.with_attention:
+          decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden, encoder_outputs)
+        else:
+          decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
         # Teacher forcing: next input is current target
         decoder_input = target_variable[t].view(1, -1)
         #Calculate and accumulate loss
