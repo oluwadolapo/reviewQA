5424d1c15b4d11c3e8e8d4bc98f56ec1002ad105
oluwadolapo <oluwadolapooni@gmail.com>
2020-10-26 00:39:33 +0100

    enabled multiple ref for bart rouge evaluation

--- test_summarizer_bart.py
+++ test_summarizer_bart.py
@@ -2,9 +2,10 @@ import torch
 from transformers import BartTokenizer, BartForConditionalGeneration
 import pandas as pd
 from rouge import Rouge
+from rouge_metric import PyRouge
 import argparse
 
-from summarizer.bart.data import prepare_data
+#from summarizer.bart.data import prepare_data
 
 def get_params():
     parser = argparse.ArgumentParser()
@@ -14,6 +15,25 @@ def get_params():
                         help='Directory for testing data')
     args = parser.parse_args()
     return args
+
+
+def prepare_data(df):
+    questions = [q if q.endswith("?") else q+"?" for q in df.question]
+    reviews = [r for r in df.passages]
+    multiple_answers = [a for a in df.multiple_answers]
+
+    questions = [q.lower() + " </s>" for q in questions]
+    reviews = [[p.lower() for p in r] for r in reviews]
+    #answers = ["<s> " + a.lower() + " </s>" for a in answers]
+    multiple_answers = [["<s> " + a.lower() + " </s>" for a in ans] for ans in multiple_answers]
+
+    quest_rev = []
+    for i, q in enumerate(questions):
+        for r in reviews[i]:
+            q = q + " " + r
+        quest_rev.append(q)
+
+    return quest_rev, multiple_answers
 
 class SummarizerModel:
   def __init__(self, output_dir):
@@ -34,15 +54,18 @@ def main():
     args = get_params()
     model = SummarizerModel(args.model_path)
     df = pd.read_json(args.data_path, orient='split')
-    df = df[49500:]
+    #df = df[:4]
     quest_rev, ref_answers = prepare_data(df)
     pred_answers = []
     for i in range(len(quest_rev)):
         answer = model.predict(quest_rev[i])[0]
         pred_answers.append(answer)
 
-    rouge = Rouge()
-    scores = rouge.get_scores(pred_answers, ref_answers, avg=True)
+    #rouge = Rouge()
+    #scores = rouge.get_scores(pred_answers, ref_answers, avg=True)
+    rouge = PyRouge(rouge_n=(1, 2, 4), rouge_l=True, rouge_w=True,
+                rouge_w_weight=1.2, rouge_s=True, rouge_su=True, skip_gap=4)
+    scores = rouge.evaluate(pred_answers, ref_answers)
 
     print(scores)
 
